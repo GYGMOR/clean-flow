b5fd1e7dc576b26130005590871b2954d4e9b793
Joel Hediger <joel.hediger@HNET.local>
2026-03-03 20:03:31 +0100

    Fix 404 errors: Multi-page build and relative links

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,18 @@
-export default {
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
     base: '/clean-flow/',
-}
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html'),
+                services: resolve(__dirname, 'services.html'),
+                news: resolve(__dirname, 'news.html'),
+                contact: resolve(__dirname, 'contact.html'),
+                impressum: resolve(__dirname, 'impressum.html'),
+            },
+        },
+    },
+})
